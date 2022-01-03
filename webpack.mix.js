c01c254220ecb5a55bd630e2d86b43a3fcaf647e
Jan Oppolzer <jan@oppolzer.cz>
2022-01-03 12:08:58 +0100

    Hide local login form by default

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,6 +3,7 @@ let mix = require('laravel-mix');
 mix
     .copy('src/index.html', 'dist/')
     .copy('src/img/', 'dist/img/')
-    .postCss('src/css/style.css', 'dist/css/', [
+    .postCss('src/css/app.css', 'dist/css/', [
         require('tailwindcss'),
-    ]);
+    ])
+    .js('src/js/app.js', 'dist/js/app.js');
